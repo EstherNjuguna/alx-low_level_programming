5dc0054db1a14dcc7950c3b94864514458a70690
EstherNjuguna <esternjuguna@gmail.com>
2022-09-15 09:02:12 -0700

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,15 @@
 #include "main.h"
+
 /**
- * print_numbers - check code
- *
- * Return: 1,2,3 \n
- *
+ *  * print_numbers - print 0 to 9
  */
+
 void print_numbers(void)
 {
 int i;
-
 for (i = 0; i <= 9; i++)
 {
 _putchar(i + '0');
 _putchar('\n');
-}
+}											}
 }

1654a92baffa331934ac46cdf4a0f83312d7a8c4
EstherNjuguna <esternjuguna@gmail.com>
2022-09-15 08:58:28 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - check code
+ *
+ * Return: 1,2,3 \n
+ *
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+_putchar('\n');
+}
+}

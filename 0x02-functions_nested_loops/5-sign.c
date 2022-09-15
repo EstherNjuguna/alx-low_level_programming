f25956fff625ee8e5458ad0a60f0c494f4a90231
EstherNjuguna <esternjuguna@gmail.com>
2022-09-14 19:10:17 -0700

    dpn

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,19 +3,24 @@
 /**
  * print_sign -check numbers
  *
- * @n : nmber
- *
- * Return: 0 or 1 or -1
- *
+ *    @n : number to check
+ *     
+ *       Return: 0 or 1 or -1 based on the result
+ *       
  */
 
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (0);
-	}
-	_putchar('-');
-	return (-1);
+		if (n > 0)
+				{
+							_putchar('+');
+									return (1);
+										}
+			if (n == 0)
+					{
+								_putchar('0');
+										return (0);
+											}
+				_putchar('-');
+					return (-1);
 }

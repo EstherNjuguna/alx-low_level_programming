751242dbc94fec405f49eb45b2ae49975699a2be
EstherNjuguna <esternjuguna@gmail.com>
2022-09-19 20:04:40 -0700

    done'

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,11 +10,19 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar("\n");
+
+
+
+
+
+
+
+while (*(str + i) != '\0')
+{
+putchar(*(str + i));
+i++;
 }
+putchar(10);
+}
+
 

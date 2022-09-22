072464fb2c98a5941125f0dd7f98dbee7100a296
EstherNjuguna <esternjuguna@gmail.com>
2022-09-22 11:08:16 -0700

    done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - integer
+ * only using putchar
+ * noarrays and pointers
+ * @n: integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		num = -n;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}

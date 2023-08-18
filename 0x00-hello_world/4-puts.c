5b4665d20e69054a30f6782e0142ea0ac3e42716
EstherNjuguna <EstherNjuguna>
2023-08-18 00:54:01 -0700

    hello wrold

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Use the puts function to print the message
+ * Return: End the program with the value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
